c0795d2b87995f4e21de50c14e37b9d8dd6e99e5
sungMH <myunghyun772@naver.com>
2020-11-28 19:32:12 +0900

    초기 커밋.

---
+++ Final_Game_project_Avengers.cpp
@@ -0,0 +1,11 @@
+﻿#include <bangtal.h>
+#include <ctime>
+#include <iostream>
+
+using namespace bangtal;
+using namespace std;
+
+int main()
+{
+    auto startScene = Scene::create("어벤져스", "images/startScene.png");
+}
